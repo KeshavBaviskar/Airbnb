be3ca7688205f9ce7e3a8f54d19f03cb7e84b05b
Keshav Baviskar <kbaviskar787@gmail.com>
2025-07-28 17:11:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express= require("express");
+const app=express();
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+
+}
+
+main().then(()=>{
+    console.log("connected to DB");
+}).catch(err=>{
+    console.log(err);
+})
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!");
+})
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+});
